9092ed0195358dc9af1215a501a2c90436ea274b
Dhanush Reddy Chilakala <dhanush.actual1@gmail.com>
2026-02-17 17:14:17 -0500

    addded bunch of logos and fixed a couple
    need to add american systems...

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,24 +1,6 @@
+import { imageLogos } from "@/data/imageLogos";
 import LogoLoop from "./ui/LogoLoop";
 
-import databricksLogo from "@/assets/logos/databricks.svg";
-import mlhLogo from "@/assets/logos/mlh.svg";
-import accentureLogo from "@/assets/logos/accenture.svg";
-import metaLogo from "@/assets/logos/meta.svg";
-import bloombergLogo from "@/assets/logos/bloomberg.svg";
-import warpLogo from "@/assets/logos/warp.svg";
-
-const imageLogos = [
-  {
-    src: databricksLogo,
-    alt: "Databricks",
-    href: "https://www.databricks.com/",
-  },
-  { src: mlhLogo, alt: "MLH", href: "https://mlh.io/" },
-  { src: accentureLogo, alt: "Accenture", href: "https://www.accenture.com/" },
-  { src: metaLogo, alt: "Meta", href: "https://about.meta.com/" },
-  { src: bloombergLogo, alt: "Bloomberg", href: "https://www.bloomberg.com/" },
-  { src: warpLogo, alt: "Warp", href: "https://www.warp.dev/" },
-];
 
 export default function Sponsors() {
   return (
---
+++ src/data/imageLogos.ts
@@ -0,0 +1,40 @@
+
+import accentureLogo from "@/assets/logos/accenture.svg";
+// import americansystemsLogo from "@/assets/logos/americansystems.svg"; // TODO: Add this back in
+import balsamiqLogo from "@/assets/logos/balsamiq.svg";
+import bloombergLogo from "@/assets/logos/bloomberg.svg";
+import capitaloneLogo from "@/assets/logos/capitalone.svg";
+import carmaxLogo from "@/assets/logos/carmax.svg";
+import costarLogo from "@/assets/logos/costar.svg";
+import databricksLogo from "@/assets/logos/databricks.svg";
+import deloitteLogo from "@/assets/logos/deloitte.svg";
+import geicoLogo from "@/assets/logos/geico.svg";
+import generalmotorsLogo from "@/assets/logos/generalmotors.svg";
+import googleLogo from "@/assets/logos/google.svg";
+import ibmLogo from "@/assets/logos/ibm.svg";
+import metaLogo from "@/assets/logos/meta.svg";
+import microsoftLogo from "@/assets/logos/microsoft.svg";
+import mlhLogo from "@/assets/logos/mlh.svg";
+import peratonLogo from "@/assets/logos/peraton.svg";
+import warpLogo from "@/assets/logos/warp.svg";
+
+export const imageLogos = [
+  { src: accentureLogo, alt: "Accenture", href: "https://www.accenture.com/" },
+  // { src: americansystemsLogo, alt: "American Systems", href: "https://www.americansystems.com/" }, // TODO: Add this back in
+  { src: balsamiqLogo, alt: "Balsamiq", href: "https://www.balsamiq.com/" },
+  { src: bloombergLogo, alt: "Bloomberg", href: "https://www.bloomberg.com/" },
+  { src: capitaloneLogo, alt: "Capital One", href: "https://www.capitalone.com/" },
+  { src: carmaxLogo, alt: "CarMax", href: "https://www.carmax.com/" },
+  { src: costarLogo, alt: "Costar", href: "https://www.costar.com/" },
+  { src: databricksLogo, alt: "Databricks", href: "https://www.databricks.com/" },
+  { src: deloitteLogo, alt: "Deloitte", href: "https://www.deloitte.com/" },
+  { src: geicoLogo, alt: "Geico", href: "https://www.geico.com/" },
+  { src: generalmotorsLogo, alt: "General Motors", href: "https://www.generalmotors.com/" },
+  { src: googleLogo, alt: "Google", href: "https://www.google.com/" },
+  { src: ibmLogo, alt: "IBM", href: "https://www.ibm.com/" },
+  { src: metaLogo, alt: "Meta", href: "https://about.meta.com/" },
+  { src: microsoftLogo, alt: "Microsoft", href: "https://www.microsoft.com/" },
+  { src: mlhLogo, alt: "MLH", href: "https://mlh.io/" },
+  { src: peratonLogo, alt: "Peraton", href: "https://www.peraton.com/" },
+  { src: warpLogo, alt: "Warp", href: "https://www.joinwarp.com/" },
+];
